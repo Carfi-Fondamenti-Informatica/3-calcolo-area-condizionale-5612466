6494b6086b275c74157a0584955abd1cf2dfcff1
5612466 <115354030+5612466@users.noreply.github.com>
2022-11-20 12:09:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,18 +7,18 @@ int main() {
    cin << a << b << opzione;
    switch (opzione){
       case 0:
-      result=((a*b)/2);
+      result = (a*b)/2;
       break;
       case 1:
-      result=(a*a);
+      result = a*a;
       break;
       case 2:
-      result=(a*b); 
+      result = a*b; 
       break;
       default:
       cout << "operazione non valida";
       break;
-   }   
+   };   
    cout << result;
    return 0;
 }
