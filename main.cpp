5bee594b5fa99a27a3ddb72bfa1b1281df174611
5612466 <115354030+5612466@users.noreply.github.com>
2022-11-20 11:49:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
+   float a=0, b=0;
+   int opzione=0;
+   cin << a << b << opzione;
+   switch (opzione){
+      case 0:
+      cout << "area triangolo= " << ((a*b)/2) << endl;
+      break;
+      case 1:
+      cout << "area quadrato= " << (a*a) << endl;
+      break;
+      case 2:
+      cout << "area rettangolo= " << (a*b) << endl; 
+      break;
+      default:
+      cout << "operazione non valida" << endl;
+      break;
+   }   
    return 0;
 }
